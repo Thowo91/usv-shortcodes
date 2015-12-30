d931b7a4189257d3d81060da207765c79419c16d
Thomas Worofsky <Thomas.Worofsky@gmail.com>
2015-12-30 09:42:00 +0100

    Add .gitignore & minimum Plugin Code

---
+++ usv-shortcodes.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:        usv-shortcodes
+Plugin URI:         https://github.com/Thowo91/usv-shortcodes
+Description:        Plugin der gesammelten Shortcodes für die Seite des Unterfränkischen Schachverbandes
+Version:            0.1
+Author:             Thomas Worofsky
+License:            GNU General Public License v2 or later
+License URI:        http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+?>
